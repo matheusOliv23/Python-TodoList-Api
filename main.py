a65f5db979988c0c0423388065c9cb466b025411
matheusoliv23 <matheushenri1996@gmail.com>
2022-07-29 21:26:12 -0300

    chore: cors configs

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+## App objeto
+app = FastAPI()
+
+origins = ['http://localhost:3000/']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+@app.get("/")
+def read_root():
+    return ("Testando")
