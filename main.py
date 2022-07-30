0d6fc40873168ba2d95d3e1bb8bab4b52d5e34c6
matheusoliv23 <matheushenri1996@gmail.com>
2022-07-29 21:37:22 -0300

    chore: add crud operations to tasks

--- main.py
+++ main.py
@@ -18,3 +18,28 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return ("Testando")
+
+
+@app.get("/api/todo")
+async def get_tasks():
+    return 1
+
+
+@app.get("/api/todo-{id}")
+async def get_task_by_id(id):
+  return 1
+
+
+@app.post("/api/todo")
+async def post_task(task):
+  return 1
+
+
+@app.put("/api/todo-{id}")
+async def put_task(id, data):
+  return 1
+
+
+@app.delete("/api/todo-{id}")
+async def delete_task(id):
+  return 1
